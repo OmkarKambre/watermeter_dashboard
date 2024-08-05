190caba34e05bfd409e3e24bfba8db85f1391808
unknown <okambre2@gmail.com>
2024-08-05 23:39:42 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import EnterPassword from './EnterPassword';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import UploadConvert from './UploadConvert';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 
 function App() {
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<><EnterPassword/></>
+  },
+
+  {
+    path:"/uploadfile",
+    element:<><UploadConvert/></>
+  },
+])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <RouterProvider router={router}/>
+    </>
   );
 }
 
