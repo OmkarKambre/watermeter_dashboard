df0d8edde51115304ff1990871111c59e3e27b45
unknown <okambre2@gmail.com>
2024-08-20 21:04:21 +0530

    BG Removed

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import imgHeader from './img_header.jpeg'; 
+import imgHeader from './og_header.png'; 
 
 const Header = () => {
-  const navigate = useNavigate(); // Initialize useNavigate hook
+  const navigate = useNavigate();
 
   const handleLogout = () => {
-    // Perform any necessary logout logic here
-    navigate('/'); // Navigate to EnterPassword page
+   
+    navigate('/'); 
   };
 
   return (
     <header className="bg-blue-600 text-white py-4 px-6 flex justify-between items-center">
-      <img src={imgHeader} alt="NxtGen Innovation" className="h-10 w-auto" />
+      <img src={imgHeader} alt="NxtGen Innovation" className="h-10 w-32" />
       <h2 className="text-lg">Water Meter Dashboard</h2>
       <div className="flex items-center">
         <span>Welcome, admin</span>
--- src/HeaderP.jsx
+++ src/HeaderP.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import imgHeader from './img_header.jpeg'; 
+import imgHeader from './og_header.png'; 
 
 const HeaderP = () => {
   return (
     <header className="bg-blue-600 text-white py-4 px-6 flex justify-between items-center">
-      <img src={imgHeader} alt="NxtGen Innovation" className="h-10 w-auto" />
+      <img src={imgHeader} alt="NxtGen Innovation" className="h-10 w-32" />
       <h2 className="text-lg">Water Meter Dashboard</h2>
       <div className="flex items-center">
-        <span>Welcome, admin</span>
+        <span>Please Login</span>
       </div>
     </header>
   );
