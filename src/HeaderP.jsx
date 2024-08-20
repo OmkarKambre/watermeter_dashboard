756b51ffa85d981504edb4141329dcee393690d1
unknown <okambre2@gmail.com>
2024-08-20 21:37:49 +0530

    widht changed

--- src/HeaderP.jsx
+++ src/HeaderP.jsx
@@ -4,7 +4,7 @@ import imgHeader from './og_header.png';
 const HeaderP = () => {
   return (
     <header className="bg-blue-600 text-white py-4 px-6 flex justify-between items-center">
-      <img src={imgHeader} alt="NxtGen Innovation" className="h-10 w-32" />
+      <img src={imgHeader} alt="NxtGen Innovation" className="h-10 w-auto" />
       <h2 className="text-lg">Water Meter Dashboard</h2>
       <div className="flex items-center">
         <span>Please Login</span>
